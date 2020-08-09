6912bf0fa5e7eafde45b190af3715cb56991284f
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-08-09 13:29:18 -0500

    Update Delevery.js

--- src/components/HomePages/Delevery.js
+++ src/components/HomePages/Delevery.js
@@ -7,7 +7,7 @@ export default function Delevery() {
         <h2>Online Menu & Fast Delivery</h2>
       </div>
       <div>
-        <p>e are adding Online service to Hong Kong Chinese Restaurant. You can now browse our latest menu online,or give us your feed back and suggestion online. We provide Fast Delivery too ($15.00 Minimum Order).</p>
+        <p>We are adding Online service to Hong Kong Chinese Restaurant. You can now browse our latest menu online,or give us your feed back and suggestion online. We provide Fast Delivery too ($15.00 Minimum Order).</p>
       </div>
       <div>
         <a>Browse Online Menu Now</a>
