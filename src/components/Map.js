699893e398fc7e34c457bd5cf73bc29df99f9c6f
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-08-10 15:14:25 -0500

    change file style on component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-
-
-export default class Map extends Component {
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 47.49855629475769,
+      lng: -122.14184416996333
+    },
+    zoom: 11
+  };
+ 
   render() {
     return (
-      <div>
-        Map
+      // Important! Always set the container height explicitly
+      <div style={{ height: '50vh', width: '100%' }}>
+        <GoogleMapReact
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
       </div>
-    )
+    );
   }
 }
+ 
+export default SimpleMap;
